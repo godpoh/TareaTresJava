5f5dd91466a3ec37dabd867c373f02c20a116102
godpoh <jeremy12troll@gmail.com>
2024-05-24 17:03:35 -0600

    Carga inicial.

--- src/tareatresjava/TareaTresJava.java
+++ src/tareatresjava/TareaTresJava.java
@@ -4,6 +4,8 @@
  */
 package tareatresjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -14,7 +16,41 @@ public class TareaTresJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        menu();
     }
-    
+
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
+        int options;
+        while (true) {
+            System.out.println("Menu");
+            System.out.println("1. Sets");
+            System.out.println("2. Votaciones");
+            System.out.println("3. Salir");
+            System.out.println("Ingrese una opcion: ");
+            options = scanner.nextInt();
+
+            switch (options) {
+                case 1:
+                    sets();
+                    break;
+                case 2:
+                    votes();
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa...");
+                    System.exit(0);
+                default:
+                    System.out.println("Digite un valor que concuerde con las opciones...");
+            }
+        }
+    }
+
+    public static void sets() {
+
+    }
+
+    public static void votes() {
+
+    }
 }
